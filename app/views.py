82915fad0579630d71a9c4d0a6cb203fc190be06
Amirzhan <amirzhan@MacBook-Air-Amirzhan.local>
2026-04-08 12:28:10 +0800

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+
+    return render(request, 'register.html')
+
+def task(request):
+
+    return render(request, 'task-create.html')
