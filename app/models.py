a197852fdcd291e138ac66abbc94007b478f9aa7
Amirzhan Albariyev <albariev123@gmail.com>
2026-04-09 23:54:27 +0800

    add Task model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    discription = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
